5e98a2e5c1cc887b24965f70936cef1e1f839cce
andicoaker <Andi@Andreas-MacBook.local>
2017-02-15 23:26:48 -0500

    inital files. plus started html, js and backbone

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,3 +4,77 @@ import $ from 'jquery';
 $.getJSON('/proxy?api=http://www.bbc.co.uk/radio1/playlist.json').then(function(serverRes){
 	console.log(serverRes)
 })
+
+$.getJSON('/proxy?api=http://www.bbc.co.uk/radio2/playlist.json').then(function(serverRes){
+	console.log(serverRes)
+})
+
+// ************* TEMPLATES *******************
+
+function bbcRadioPlaylistTemplate(listOfPlaylistModels, viewType, dataObj){
+	// let viewTypeTitleMap = {
+	// 	root: 'All the Congress People',
+// 		byState: `Showing Cuties from ${dataObj.state}`,
+// 		byGenderAndParty: `Showing ${dataObj.party} Cuties that are ${dataObj.gender}`
+// 	}
+//
+// 	let congressPeopleHtml = listOfCongressFolkModels.map( function(legislatorModel, viewType, dataObj){
+// 	return `
+// 		<div class="profile-card">
+// 			<img src="http://flathash.com/${legislatorModel.get('bioguide_id')}">
+// 			<h5>
+// 				${legislatorModel.get('first_name')}</br>
+// 				<small>${legislatorModel.get('state_name')}</small>
+// 			</h5>
+// 		</div>`
+//
+// }
+
+// ************* ROUTER *************
+
+const AppRouter = Backbone.Router.extend({
+  initialize: function(){
+      console.log('app routing');
+      Backbone.history.start()
+  }
+
+	routes : {
+		'playlist/:a' : 'showRadio2',
+		'playlist/:b' : 'showRadio1',
+		' " "/:introducing' : 'showHome'
+	},
+
+	showHome: function(){
+		let bbcRadioCollection = new bbcRadioCollection()
+		bbcRadioCollection.fetch().then(function(serverRes){
+
+			let playlistModelsList = bbcRadioCollection.models
+			let bigHTMLStr = bbcRadioHomeTemplate(playlistModelsList, 'root', {})
+			document.querySelector('#app-container').innerHTML = bigHTMLStr
+		})
+
+	},
+
+	showRadio1: function(playlist1Route){
+		let bbcRadioColl = new bbcRadioCollection(`playlist=${playlist1Route}`)
+		bbcRadioColl.fetch().then(function(serverRes){
+
+			let playlistModelsList = bbcRadioColl.models
+			let bigHTMLStr = bbcRadioPlaylistTemplate(playlistModelsList, 'root', {})
+			document.querySelector('#app-container').innerHTML = bigHTMLStr
+		})
+
+	}),
+
+	showRadio2: function(playlist1Route){
+		let bbcRadioColl = new bbcRadioCollection(`playlist=${playlist2Route}`)
+		bbcRadioColl.fetch().then(function(serverRes){
+
+			let playlistModelsList = bbcRadioColl.models
+			let bigHTMLStr = bbcRadioPlaylistTemplate(playlistModelsList, 'root', {})
+			document.querySelector('#app-container').innerHTML = bigHTMLStr
+		})
+
+	}),
+
+const myApp = new AppRouter()
---
+++ src/scripts/models/models.js
@@ -0,0 +1,20 @@
+//
+// const bbcRadioModel = Backbone.Model.extend({
+// })
+//
+// const bbcRadioCollection = Backbone.Collection.extend({
+// 	initialize: function(qryStrParams){
+// 		if(typeof qryStrParams !== 'undefined'){
+// 			this.url = `${this.url}&${qryStrParams}`
+// 		}
+// 	},
+//
+// 	parse: function(rawServerRes){
+// 		console.log('parsing Response!!')
+// 		return rawServerRes.results
+// 	},
+//
+// 	url: 'http://www.bbc.co.uk/?callback=?',
+//
+// 	model: bbcRadioModel
+// })
